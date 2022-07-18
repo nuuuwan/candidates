4d479cc045cc99adc5c4df60e3adc7405e8463af
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-07-18 15:14:08 +0530

    Added WeightView

---
+++ src/view/molecules/WeightView.js
@@ -0,0 +1,18 @@
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import Weight from "../../nonview/core/Weight"
+import {t} from "../../nonview/base/I18N"
+
+export default function WeightView({weight}) {
+  
+  const color = Weight.getColor(weight);
+
+  return (
+    <Stack direction="column" sx={{ textAlign: "right" }} color={color}>
+      <Typography variant="h6">{Weight.signed(weight)}</Typography>
+      <Typography variant="caption">
+        {t(Weight.getQualitativeText(weight))}
+      </Typography>
+    </Stack>
+  );
+}
