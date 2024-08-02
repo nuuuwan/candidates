c09a4c9a4a2f54525328706fd80f21d6b37d9a3b
nuuuwan <nuuuwan@gmail.com>
2024-08-02 16:39:39 +0530

    More data

--- src_py/gen_all.py
+++ src_py/gen_all.py
@@ -1,12 +1,12 @@
 import gen_candidates
 import gen_criteria
-import src_py.gen_criterion_to_candidate_to_weight as gen_criterion_to_candidate_to_weight
+import src_py.gen_criterion_to_candidate_to_weight_info as gen_criterion_to_candidate_to_weight_info
 
 
 def main():
     gen_candidates.main()
     gen_criteria.main()
-    gen_criterion_to_candidate_to_weight.main()
+    gen_criterion_to_candidate_to_weight_info.main()
 
 
 if __name__ == "__main__":
